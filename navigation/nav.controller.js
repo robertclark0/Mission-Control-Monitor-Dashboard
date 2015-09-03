7622cecb03d265cfb2b171bc1e5ad098a152de3b
Robert Clark <robertclark0@gmail.com>
2015-09-03 11:44:33 -0500

    Monitor LI background change on selected.

--- navigation/nav.controller.js
+++ navigation/nav.controller.js
@@ -1,5 +1,10 @@
 mcmdApp.controller('NavController',['$scope', 'currentMonitorsService',function($scope, currentMonitorsService){
     $scope.monitorList = currentMonitorsService.monitorList;
 
+    $scope.checkValue = 0;
 
+    $scope.checkIndex = function (index) {
+        $scope.checkValue = index;
+        console.log($scope.checkValue);
+    };
 }]);
